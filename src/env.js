a51d374cc50b27901918deb5352758d4771c1de2
Stefan Rimaila <stefan@rimaila.fi>
2018-10-09 19:39:45 +0300

    Add additional validators

--- src/env.js
+++ src/env.js
@@ -1,9 +1,22 @@
+/**
+ * System environment checking
+ *
+ * Check that the environment is properly set up to ensure
+ * operation.
+ */
+
 const R = require('ramda');
 const V = require('partial.lenses.validation');
 
+const urlRegex = /mongodb:\/\/\w+:\d+/;
+
 const isNonEmpty = R.identity;
+const isLengthWithin = (min, max) => R.compose(R.both(R.gte(min), R.lte(max)), R.length);
+const isUrl = R.test(urlRegex);
 
 const isRequired = [isNonEmpty, 'required'];
+const isPrefixValidLength = [isLengthWithin(1, 2), 'prefixTooShort'];
+const isValidUrl = [isUrl, 'invalidUrl'];
 
 const envRules = V.propsOr(V.remove, {
   DISCORD_TOKEN: V.and(
@@ -11,12 +24,12 @@ const envRules = V.propsOr(V.remove, {
   ),
   DISCORD_COMMAND_PREFIX: V.and(
     isRequired,
+    isPrefixValidLength,
   ),
-  GOOGLE_KEY: V.and(
-    isRequired,
-  ),
+  GOOGLE_KEY: isRequired,
   MONGODB_URL: V.and(
     isRequired,
+    isValidUrl,
   ),
 });
 
